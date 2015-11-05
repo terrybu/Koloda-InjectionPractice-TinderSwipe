b12112c4f633d4bc5f369ad478401b638c66d3a4
Terry Bu <burea1124@gmail.com>
2015-11-05 07:20:46 -0500

    Correctly expanding card view on click and shrinking it back on second click

--- Practice-Koloda-Injection/MainViewController.swift
+++ Practice-Koloda-Injection/MainViewController.swift
@@ -17,7 +17,10 @@ class MainViewController: UIViewController, KolodaViewDataSource, KolodaViewDele
     @IBOutlet weak var kolodaView: KolodaView!
     var imageNamesArray: [String] = ["pestoPasta", "burger", "wineTasting", "dragonRoll", "billGates", "jdFace"]
     
-    
+    var expandedClick: Bool = false
+    var originalFrame: CGRect?
+    var originalCardFrame: CGRect?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -74,9 +77,32 @@ class MainViewController: UIViewController, KolodaViewDataSource, KolodaViewDele
     
     func kolodaDidSelectCardAtIndex(koloda: KolodaView, index: UInt) {
         print("card tapped at index \(index)")
+//        
+//        let modalVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("ModalVC")
+//        presentViewController(modalVC, animated: true, completion: nil)
         
-        let modalVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("ModalVC")
-        presentViewController(modalVC, animated: true, completion: nil)
+        
+        if expandedClick == false {
+            originalFrame = self.kolodaView.frame
+            originalCardFrame = self.kolodaView.viewForCardAtIndex(0)!.frame
+            UIView.animateWithDuration(0.5, animations: { () -> Void in
+                self.kolodaView.frame = CGRectMake(0, 0, self.view.frame.width, self.kolodaView.frame.height)
+                self.kolodaView.viewForCardAtIndex(0)!.frame = CGRectMake(0, 0, self.view.frame.width, self.kolodaView.frame.height)
+                
+                }) { (finished) -> Void in
+                    //finish
+                    self.expandedClick = true
+            }
+        } else if expandedClick == true{
+            UIView.animateWithDuration(0.5, animations: { () -> Void in
+                self.kolodaView.frame = self.originalFrame!
+                self.kolodaView.viewForCardAtIndex(0)!.frame = self.originalCardFrame!
+                }) { (finished) -> Void in
+                    self.expandedClick = false
+            }
+        }
+    
+        
     }
     
     func kolodaShouldApplyAppearAnimation(koloda: KolodaView) -> Bool {
